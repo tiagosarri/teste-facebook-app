4133fbdbc6e01e4f2b94716693c2f4566584ec9c
Tiago Sarri <tiago.sarri@fireflyon.com>
2012-06-12 20:06:55 -0300

    Ajuste

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ enable :sessions
 set :raise_errors, false
 set :show_exceptions, false
 
+
 # Scope defines what permissions that we are asking the user to grant.
 # In this example, we are asking for the ability to publish stories
 # about using the app, access to what the user likes, and to be able
